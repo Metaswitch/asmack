342bc2dcc3525708e5f58ed5c74e1d8c965e683c
Mark Berry <meb@metaswitch.com>
2013-05-29 12:32:29 +0100

    Resolve merge problems

--- patch/11-remove-nls-harmony.sh
+++ patch/11-remove-nls-harmony.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
-find org/apache/harmony -name '*.java' -exec sed -i '' 's:import org.apache.harmony.auth.internal.nls.Messages;::' '{}' ';'
-find org/apache/harmony -name '*.java' -exec sed -i '' 's:Messages.getString(\("[^"]*"\)):\1:g' '{}' ';'
-=======
 [ "Darwin" = $(uname) ] && SEDI="sed -i ''" || SEDI='sed -i'
 
 find org/apache/harmony -name '*.java' -exec $SEDI 's:import org.apache.harmony.auth.internal.nls.Messages;::' '{}' ';'
 find org/apache/harmony -name '*.java' -exec $SEDI 's:Messages.getString(\("[^"]*"\)):\1:g' '{}' ';'
->>>>>>> 18a81494e8f2752740f2e450cb2b024aeafd3bc5
 
--- patch/40-enable-custom-sasl.sh
+++ patch/40-enable-custom-sasl.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
-find . -name '*.java' -exec sed -i '' 's:import org.apache.harmony.javax.security.sasl.Sasl;:import de.measite.smack.Sasl;:g' '{}' ';'
-=======
 [ "Darwin" = $(uname) ] && SEDI="sed -i ''" || SEDI='sed -i'
 
 find . -name '*.java' -exec $SEDI 's:import org.apache.harmony.javax.security.sasl.Sasl;:import de.measite.smack.Sasl;:g' '{}' ';'
->>>>>>> 18a81494e8f2752740f2e450cb2b024aeafd3bc5
 
